38fa11c2a35e61af17c45b928759a7ddfdfb0d15
krys jonaz <c.johnsonbl@gmail.com>
2015-05-21 23:12:29 -0500

    ???

---
+++ public_html/showHideDiv.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var curDiv;
+
+function showDiv(id, school) {
+    //hide current div
+    if (curDiv !== null) {
+        document.getElementById(curDiv).style.display = "none";
+    }
+    document.getElementById(id).style.display = "block";
+    showSchool(school);
+    window.scrollByLines(500);
+    curDiv = id;
+}
+
+function showSchool(school){
+     document.getElementById(school).style.display="none";
+}
