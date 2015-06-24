281d743df49023ea9a5cbd2a077233b389623fc0
krys jonaz <c.johnsonbl@gmail.com>
2015-06-23 23:46:25 -0500

    FIXED ERR

---
+++ TEDxUniversityOfWinnipeg/public_html/public_html/js/showHideDiv.js
@@ -0,0 +1,32 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var curDiv;
+
+function showDiv(id) {
+    //hide current div
+    if (curDiv != null) {
+        document.getElementById(curDiv).style.display = "none";
+    }
+    document.getElementById(id).style.display = "inline";
+    curDiv = id;
+}
+function showSchool(school) {
+    document.getElementById(school).style.display = "none";
+}
+
+$(document).ready(function(){
+  $("#hide").click(function(){
+    $("#page").hide();
+  });
+
+  $("#show").click(function(){
+    $("#page").show();
+  });
+
+});
+
