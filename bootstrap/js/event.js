5f30b4ced7025c53465bd4723c0e01f4b840b785
krys jonaz <c.johnsonbl@gmail.com>
2015-07-07 05:27:15 -0500

    fixed up showhidediv.js

--- bootstrap/js/event.js
+++ bootstrap/js/event.js
@@ -1,7 +1,7 @@
 
 
 window.onload = function () {
-    var liElems = document.getElementById("navbar-ul").childNodes;
+    var liElems = document.getElementById("navbar-ul").child
 
     for (var i = 0; i < liElems; i++) {
         document.addEventListener("click", document.getElementById(liElems[i] + "-div") , false);
