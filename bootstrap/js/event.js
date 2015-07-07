ab456af16b3c6f04431b6a4f57c16163b404cedb
krys jonaz <c.johnsonbl@gmail.com>
2015-07-07 07:24:53 -0500

    ARRRRGH

--- bootstrap/js/event.js
+++ bootstrap/js/event.js
@@ -4,7 +4,8 @@ window.onload = function () {
     var liElems = document.getElementById("navbar-ul").child;
 
     for (var i = 0; i < liElems; i++) {
-        document.addEventListener("click", document.getElementById(liElems[i].innerHTML + "-div") , false);
-        showDiv
+        var divToAddEventTo =  document.getElementById(liElems[i].innerHTML) + "-div";
+        document.addEventListener("click", divToAddEventTo, false);
+        showDiv(divToAddEventTo);
     }
 };
