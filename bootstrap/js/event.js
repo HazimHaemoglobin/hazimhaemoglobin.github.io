aa609e8a647d150fa78f8d6a891e7cddcf7076e1
krys jonaz <c.johnsonbl@gmail.com>
2015-07-07 07:29:43 -0500

    '

--- bootstrap/js/event.js
+++ bootstrap/js/event.js
@@ -9,3 +9,25 @@ window.onload = function () {
         showDiv(divToAddEventTo);
     }
 };
+
+
+
+var curDiv;
+
+function showDiv(id) {
+    //hide current div
+    if (curDiv !== null) {
+        document.getElementById(curDiv).style.display = "none";
+    }
+   
+    document.getElementById(id).style.display = "inline";
+    curDiv = id;
+    changePageTitle(id);
+}
+
+
+function changePageTitle(title) {
+    var updatedTitle = title.split("-");
+    window.location.hash = "http://www.tedxuniversityofwinnipeg.com/" + updatedTitle[0];
+    
+}
