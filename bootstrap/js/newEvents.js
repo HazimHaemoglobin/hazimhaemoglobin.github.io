42ec593d437d5000a56ff167b88731177b887e1a
krys jonaz <c.johnsonbl@gmail.com>
2015-07-13 16:27:46 -0500

    fucking around lmbo

--- bootstrap/js/newEvents.js
+++ bootstrap/js/newEvents.js
@@ -1,47 +1,26 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+'use strict';
 
 (function (w, d) {
-    console.log("START FUNCTION");
-    var allElements = d.getElementsByTagName("*");
-    console.log(allElements);
-    var allIds = [];
-    console.log(allIds);
-    for (var i = 0, n = allElements.length; i < n; ++i) {
-        var el = allElements[i];
-        if (el.id) {
-            allIds.push(el.id);
+
+    var prevId;
+    var handler = function(event){
+        console.log('event', event);
+
+        if(event.oldURL === event.newURL) return;
+
+        var id = w.location.hash;
+
+        $(id).fadeIn(400);
+
+        if(prevId){
+            $(prevId).fadeOut(400);
         }
-    }
-    console.log(allIds);
 
-    oldHash = w.location.hash;
-    console.log(oldHash);
-
-    w.onhashchange = function () {
-        console.log("INSIDE ONHASHCHANGE METHOD");
-        if (oldHash === w.location.hash)
-            return;
-
-        allIds.forEach(function (id) {
-            id = "#" + id;
-            console.log(id);
-            if (id === w.location.hash) {
-                $(".content div:not(.hidden)").fadeOut(400, function () {
-                    $(".content div:not(.hidden)").addClass("hidden");
-                    $(id).fadeIn(400, function () {
-                        $(id).removeClass("hidden");
-
-                    });
-                });
-            }
-        });
-
-        oldHash = w.location.hash;
+        prevId = id;
     };
+
+    w.addEventListener("hashchange", handler, false);
+
+    $('.hidden').hide();
+
 }(this, this.document));
