e1ef281039939d1a423dfabe0e19aac94d5773ce
krys jonaz <c.johnsonbl@gmail.com>
2015-07-15 12:04:38 -0500

    ayy

--- bootstrap/js/newEvents.js
+++ bootstrap/js/newEvents.js
@@ -3,29 +3,33 @@
 (function (w, d) {
 
     var prevId;
-    var handler = function(event){
+    var handler = function (event) {
         $('.tedx-logo').hide();
         console.log('event', event);
 
-        if(event.oldURL === event.newURL) return;
+        if (event.oldURL === event.newURL)
+            return;
 
         var id = w.location.hash;
 
         $(id).fadeIn(400);
         console.log("id is ", id);
 
-        if(prevId){
+        if (prevId) {
             $(prevId).fadeOut(400);
         }
 
         prevId = id;
     };
 
-    w.addEventListener("hashchange", handler, false);
+    w.addEventListener("hashchange", function () {
+        $('.body-background-image').fadeTo(1, 0.5,
+                handler());
+    }, false);
 
     $('.nav-content-hidden').hide();
-    if(event.newUrl.contains("home")){
-        $('.tedx-logo').show();
+    if (event.newUrl.contains("home")) {
+        $('.tedx-logo').fadeIn();
     }
 
 }(this, this.document));
